47cde0c3f66749adccdc4ffcf75b1c549b880236
wolf-math <aaron.wolf@gmail.com>
2020-06-14 10:14:43 +0300

    cleanup

--- src/pages/homepage/homepage.page.jsx
+++ src/pages/homepage/homepage.page.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
+import './homepage.style.scss';
+
+import logo from '../../assets/wolf-logo_D5.png';
+
 const HomePage = () => (
-    <div>
-        what's up bro
+    <div className='homepage'>
+        <div className="mainlogo">
+            <img id='logo' src={logo} alt="Logo" />;
+            <div className="title">
+                <h1>Aaron Wolf</h1>
+                <h3>front end developer</h3>
+            </div>
+        </div>
 
     </div>
 )
