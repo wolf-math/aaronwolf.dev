37b5f08433e61fab50b8a755d2d89ca2013d6aca
wolf-math <aaron.wolf@gmail.com>
2020-07-09 17:44:42 +0300

    about page

--- src/pages/about/about.page.jsx
+++ src/pages/about/about.page.jsx
@@ -4,7 +4,7 @@ import './about.style.scss';
 
 import Navbar from '../../components/navbar/navbar.component';
 
-import { FaTerminal, FaDiamond, FaPython, FaReact } from 'react-icons/fa';
+import { FaTerminal, FaPython, FaReact, FaLinux } from 'react-icons/fa';
 
 const About = () => (
     <>
@@ -13,7 +13,7 @@ const About = () => (
             <h1>About <span class='highlight'>Aaron</span></h1>
 
             <div class="container">
-                <p>I am a full stack developer. I excel at understanding the needs of client. I'm a quick learner who can work on a wide variety of projects, and I'm able to work on existing projects or creating new projects from scratch.</p>
+                <p>I am a full stack developer with a focus on front end. I'm a quick learner who can work on a wide variety of projects, and I'm able to work on existing projects or creating new projects from scratch.</p>
 
                 <p>Prior to being a full stack developer I was a high school Computer Programming and Mathematics teacher in Cleveland, Ohio. I used to teach mathematics from a programming perspective with the help of my favorite tool, the <a href="http://www.raspberrypi.org">Raspberry Pi</a>.</p>
 
@@ -22,20 +22,20 @@ const About = () => (
                 <p>Are you looking for a develoer? I'm for hire!</p>
             </div>
 
-            <h1><i class="fas fa-terminal"></i> Coding <span class="highlight">Languages</span></h1>
+            <h1><FaTerminal /> Coding <span class="highlight">Languages</span></h1>
             <div class="languages">
                 <div class="language">
-                    <h5><i class="fab fa-html5"></i> Web Dev</h5>
+                    <h5><FaReact /> Web Dev</h5>
                     <ul class="skills">
                         <li>ReactJS</li>
                         <li>Firebase</li>
                         <li>HTML5 & CSS3</li>
-                        <li>Bootstrap</li>
                         <li>SASS/SCSS</li>
+                        <li>Ruby on Rails</li>
                     </ul>
                 </div>
                 <div class="language">
-                    <h5><i class="fab fa-python"></i> Python</h5>
+                    <h5><FaPython /> Python</h5>
                     <ul class="skills">
                         <li>Django</li>
                         <li>MatPlotLib</li>
@@ -46,18 +46,11 @@ const About = () => (
                     </ul>
                 </div>
                 <div class="language">
-                    <h5><i class="fa fa-diamond"></i> Ruby</h5>
-                    <ul class="skills">
-                        <li>Ruby on Rails</li>
-                        <li>Webpacker</li>
-                    </ul>
-                </div>
-                <div class="language">
                     <h5><i class="fas fa-microchip"></i> Other Technologies</h5>
                     <ul class="skills nobullet">
                         <li><i class="fab fa-wordpress"></i> Wordpress</li>
-                        <li><i class="fab fa-linux"></i> Linux</li>
-                        <li><i class="fas fa-database"></i> PostgreSQL</li>
+                        <li><FaLinux />Linux</li>
+                        <li><i class="fas fa-database"></i> SQL</li>
                     </ul>
                 </div>
             </div>
