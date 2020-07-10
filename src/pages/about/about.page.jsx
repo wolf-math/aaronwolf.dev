26136c9056614fe22b7db430039997b85a716eed
wolf-math <aaron.wolf@gmail.com>
2020-07-10 18:03:52 +0300

    fixed typo in about

--- src/pages/about/about.page.jsx
+++ src/pages/about/about.page.jsx
@@ -15,13 +15,13 @@ const About = () => (
             <h1>About <span class='highlight'>Aaron</span></h1>
 
             <div class="container">
-                <p>I am a full stack developer with a focus on front end, looking for a job in Israel.</p>
+                <p>I am a full stack developer with a focus on front end, looking for a job in Israel or remotely.</p>
 
                 <p>Prior to being a full stack developer I was a high school Computer Programming and Mathematics teacher in Cleveland, Ohio. I used to teach mathematics from a programming perspective with the help of my favorite tool, the <a href="http://www.raspberrypi.org">Raspberry Pi</a>.</p>
 
                 <p>You can find most of my lesson plans on this website (or Github). They are in the form of LaTeX code and they are, slowly but surely, being converted into Jupyter Notebooks</p>
 
-                <p>Are you looking for a develoer? I'm for hire!</p>
+                <p>Are you looking for a developer? I'm for hire!</p>
             </div>
 
             <h1><FaTerminal /> Coding <span class="highlight">Languages</span></h1>
