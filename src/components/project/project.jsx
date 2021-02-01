9d6ca16290386783616c583b7e7e507d7efc188c
aaron <aaron.wolf@gmail.com>
2021-02-01 17:30:46 +0200

    portfolio page

--- src/components/project/project.jsx
+++ src/components/project/project.jsx
@@ -5,15 +5,24 @@ import './project.scss';
 const Project = (props) => {
 
     // remove dashes and underscores and capitalize titles.
-    const title = props.title.charAt(0).toUpperCase() + props.title.slice(1).replace(/-/g, " ").replace(/_/g, " ");    
+    const title = props.title.charAt(0).toUpperCase() + props.title.slice(1).replace(/-/g, " ").replace(/_/g, " ");
+
+    // truncate description. 
+    const description = (props.description === null ? " " : (        
+        props.description.length > 80 ? 
+        props.description.slice(0, 80) + "..." : 
+        props.description))
 
     return (
-        <div className="project">
-            <a href={props.url} target="_blank" rel="noopener noreferrer">
-                <h3>{title}</h3>
-                <p>{props.description}</p>
-            </a>
-        </div>
+        <a href={props.url} target="_blank" rel="noopener noreferrer">
+            <div className="project">
+                <div className="title">
+                    <h3>{title}</h3>
+                </div>
+                    <p>{description}</p>
+                    <p>Written in {props.language}</p>
+            </div>
+        </a>
     )
 }
 
--- src/pages/portfolio/portfolio.page.jsx
+++ src/pages/portfolio/portfolio.page.jsx
@@ -23,9 +23,11 @@ const Portfolio = () => {
             <h1>Portfolio</h1>
 
             <div className="projects">
+                {/* <Project title="This is a really really really really long title" description="One time I hired a monekey to take notes for me in class. I would just sit there with my mind a complete blank while the monkey would scribble on little pieces of paper" /> */}
             {repos.map(repo => <Project key={repo.id} 
                 title={repo.name} 
-                description={repo.description} 
+                description={repo.description}
+                language={repo.language}
                 url={repo.html_url}
                 />)}
             </div>
