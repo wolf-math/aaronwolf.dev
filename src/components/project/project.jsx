26f004da5f0743db611edd338eb904f06bb4ed70
aaron <aaron.wolf@gmail.com>
2021-02-10 11:11:21 +0200

    update project for best practices

--- src/components/project/project.jsx
+++ src/components/project/project.jsx
@@ -4,31 +4,31 @@ import Roll from 'react-reveal/Roll';
 import './project.scss';
 import Card from 'react-bootstrap/Card';
 
-const Project = (props) => {
+const Project = ({title, description, language, url, created, updated}) => {
 
     // remove dashes and underscores and capitalize titles.
-    const title = props.title.charAt(0).toUpperCase() + props.title.slice(1).replace(/-/g, " ").replace(/_/g, " ");
+    title = title.charAt(0).toUpperCase() + title.slice(1).replace(/-/g, " ").replace(/_/g, " ");
 
     // truncate description. 
-    const description = (props.description === null ? " " : (        
-        props.description.length > 70 ? 
-        props.description.slice(0, 70) + "..." : 
-        props.description))
+    description = (description === null ? " " : (        
+        description.length > 70 ? 
+        description.slice(0, 70) + "..." : 
+        description))
 
     // use this if you want more date formatting https://www.w3schools.com/jsref/jsref_getmonth.asp
 
-    const created = new Date(props.created)
+    created = new Date(created)
     const dateCreated = `${created.getFullYear()}-${created.getMonth()+1}-${created.getDate()}`;
 
-    const updated = new Date(props.updated);
+    updated = new Date(updated);
     const dateUpdated = `${updated.getFullYear()}-${updated.getMonth()+1}-${updated.getDate()}`;
 
     return (
         <Roll bottom >
             <Card style={{ width: '60%' }} bg='dark' text='light'>
-                <Card.Header><Card.Subtitle>{props.language}</Card.Subtitle></Card.Header>
+                <Card.Header><Card.Subtitle>{language}</Card.Subtitle></Card.Header>
                 <Card.Body>
-                    <a href={props.url} target="_blank" rel="noopener noreferrer"><Card.Title>{title}</Card.Title></a>
+                    <a href={url} target="_blank" rel="noopener noreferrer"><Card.Title>{title}</Card.Title></a>
                     <Card.Text>{description}</Card.Text>
                     <p className="timestamp">updated: {dateUpdated}</p>
                     <p className="timestamp">created: {dateCreated}</p>
