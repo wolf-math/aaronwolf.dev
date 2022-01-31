9d87e1d7dcf7dbfff87eaa6894c495c3f9c6c77c
aaron <aaron.wolf@gmail.com>
2022-01-31 08:50:59 +0200

    carousel edits

--- src/components/project/work.jsx
+++ src/components/project/work.jsx
@@ -2,19 +2,20 @@ import React from "react";
 
 import "./work.scss";
 
-const Work = (id, name, url, github, image, languages) => {
+const Work = ({key, name, url, github, image, languages}) => {
     return (
-        <div className="work">
+        <div className="work" key={key}>
             <div className="image-cap">
-                <img src={image} />
+                <img src={image} alt={name} />
             </div>
             <div className="text">
                 <h3>{name}</h3>
                 <h2>{url}</h2>
                 <h2>{github}</h2>
                 <h2>{languages}</h2>
-
             </div>
         </div>
     )
 }
+
+export default Work
--- src/pages/portfolio/portfolio.page.jsx
+++ src/pages/portfolio/portfolio.page.jsx
@@ -2,75 +2,84 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios'; 
 
 import './portfolio.style.scss'
-import {Jumbotron, Container, Carousel} from 'react-bootstrap';
+import {Jumbotron} from 'react-bootstrap';
+import Carousel from 'react-bootstrap/Carousel';
+  
 
 import Navbar from '../../components/navbar/navbar.component';
-import Project from '../../components/project/project';
-import Post from '../../components/post/post.component';
+// import Work from '../../components/project/work';
+// import Post from '../../components/post/post.component';
 
 
 const Portfolio = () => {
 
-    // const [repos, setRepos] = useState([])
+    const [repos, setRepos] = useState([])
 
-    // useEffect(() => {
-    //     axios.get("https://api.github.com/users/wolf-math/repos").then((res)=>{
-    //       setRepos(res.data.filter(repo => repo.fork===false).sort((a,b) => new Date(b.updated_at) - new Date(a.updated_at)));
-    //     })
-    //   }, [])
+    useEffect(() => {
+        axios.get("https://api.github.com/users/wolf-math/repos").then((res)=>{
+          setRepos(res.data.filter(repo => repo.fork===false).sort((a,b) => new Date(b.updated_at) - new Date(a.updated_at)));
+        })
+      }, [])
 
-    const projects = [
-        {
-            "id": 1,
-            "name": "Aaron.Wolf.dev",
-            "url": "https://aaronwolf.dev",
-            "github": "https://github.com/wolf-math/aaronwolf.dev",
-            "image": "../../assets/wolfdev.png",
-            "languages": "JavaScript - ReactJS"
-        },
-        {
-            "id": 2,
-            "name": "Caya Coffee Co",
-            "url": "https://cayacoffee.co",
-            "github": "https://github.com/wolf-math/coffeeshop",
-            "image": "../../assets/caya.png",
-            "languages": "JavaScript - ReachJS"
-        },
-        {
-            "id": 3,
-            "name": "Swish",
-            "url": "http://justswish.com",
-            "github": "https://github.com/wolf-math/swish",
-            "image": "../../assets/swish.png",
-            "languages": "Ruby on Rails"
-        },
-        {
-            "id": 4,
-            "name": "Cleveland Karate",
-            "url": "https://clevelandshotokan.wordpress.com/",
-            "image": "",
-            "github": "",
-            "languages": "Wordpress"
-        }
-    ]
+    // const projects = [
+    //     {
+    //         "id": 1,
+    //         "name": "Aaron.Wolf.dev",
+    //         "url": "https://aaronwolf.dev",
+    //         "github": "https://github.com/wolf-math/aaronwolf.dev",
+    //         "image": "../../assets/wolfdev.png",
+    //         "languages": "JavaScript - ReactJS"
+    //     },
+    //     {
+    //         "id": 2,
+    //         "name": "Caya Coffee Co",
+    //         "url": "https://cayacoffee.co",
+    //         "github": "https://github.com/wolf-math/coffeeshop",
+    //         "image": "../../assets/caya.png",
+    //         "languages": "JavaScript - ReachJS"
+    //     },
+    //     {
+    //         "id": 3,
+    //         "name": "Swish",
+    //         "url": "http://justswish.com",
+    //         "github": "https://github.com/wolf-math/swish",
+    //         "image": "../../assets/swish.png",
+    //         "languages": "Ruby on Rails"
+    //     },
+    //     {
+    //         "id": 4,
+    //         "name": "Cleveland Karate",
+    //         "url": "https://clevelandshotokan.wordpress.com/",
+    //         "image": "",
+    //         "github": "",
+    //         "languages": "Wordpress"
+    //     }
+    // ]
+// const Work = (id, name, url, github, image, languages) 
 
     return(
         <>
             <Navbar />
 
-            <Jumbotron fluid>
-            <Container>
+            <Jumbotron>
                 <h1>Portfolio</h1>
                 <h3>My GitHub projects.</h3>
-            </Container>
             </Jumbotron>
             
             <Carousel>
-                <Carousel.Item interval={1000}>
-                    <Carousel.Caption>
-                        
-                    </Carousel.Caption>
-                </Carousel.Item>
+
+                {repos.map((repo)=> (
+                    <Carousel.Item>
+                        <div className="carouselCap">
+                            <a href={repo.html_url} >
+                            <h3>{repo.name}</h3>
+                            <p>{repo.description}</p>
+                            {/* <Carousel.Caption>
+                            </Carousel.Caption> */}
+                            </a>
+                        </div>
+                  </Carousel.Item>
+                ))}
             </Carousel>
         </>
     )
